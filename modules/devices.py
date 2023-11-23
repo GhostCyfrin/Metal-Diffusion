0ba2d8983bebe89122acfb508623ddfa117ae262
Cyfrin <114883761+GhostCyfrin@users.noreply.github.com>
2023-11-23 16:49:10 -0700

    Add files via upload

--- modules/devices.py
+++ modules/devices.py
@@ -60,7 +60,8 @@ def enable_tf32():
 
         # enabling benchmark option seems to enable a range of cards to do fp16 when they otherwise can't
         # see https://github.com/AUTOMATIC1111/stable-diffusion-webui/pull/4407
-        if any(torch.cuda.get_device_capability(devid) == (7, 5) for devid in range(0, torch.cuda.device_count())):
+        device_id = (int(shared.cmd_opts.device_id) if shared.cmd_opts.device_id is not None and shared.cmd_opts.device_id.isdigit() else 0) or torch.cuda.current_device()
+        if torch.cuda.get_device_capability(device_id) == (7, 5) and torch.cuda.get_device_name(device_id).startswith("NVIDIA GeForce GTX 16"):
             torch.backends.cudnn.benchmark = True
 
         torch.backends.cuda.matmul.allow_tf32 = True
@@ -75,11 +76,11 @@ device_interrogate: torch.device = None
 device_gfpgan: torch.device = None
 device_esrgan: torch.device = None
 device_codeformer: torch.device = None
-dtype: torch.dtype = torch.float16
-dtype_vae: torch.dtype = torch.float16
-dtype_unet: torch.dtype = torch.float16
-unet_needs_upcast = False
-attn_needs_upcast = False
+dtype: torch.dtype = torch.float64
+dtype_vae: torch.dtype = torch.float64
+dtype_unet: torch.dtype = torch.float64
+unet_needs_upcast = True
+attn_needs_upcast = True
 
 
 def cond_cast_unet(input):
